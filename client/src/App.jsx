53faa6ec13f7537b2f888c9dba5a07b2a747e78f
SteveKamoni <stevekamoni4@gmail.com>
2025-05-28 18:47:44 +0300

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className="min-h-screen flex items-center justify-center text-xl font-semibold">
+      Job Tracker App
+    </div>
+    </>
+  )
+}
+
+export default App
