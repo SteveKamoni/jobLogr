297ba557b172acc7d18d102c0aca624f204d45d8
SteveKamoni <stevekamoni4@gmail.com>
2025-05-29 22:43:34 +0300

    Day 2 commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <>
      <div className="min-h-screen flex items-center justify-center text-xl font-semibold">
       Job Tracker App
+      {/* Day 29 commit */}
     </div>
     </>
   )
